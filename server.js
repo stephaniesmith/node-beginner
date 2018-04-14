51ac4929587c19659da57c3e13c75f5d9aa7e41c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-14 13:56:35 -0700

    wrapped server in a start function to export

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const http = require('http');
 
-// http.createServer(function(request, response) {
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
-//     response.write('hello world');
-//     response.end();
-// }).listen(8888);
+function start() {
+    function onRequest(request, response) {
+        console.log('Request received');
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('hello world');
+        response.end();
+    }
 
-function onRequest(request, response) {
-    console.log('Request received');
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world');
-    response.end();
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+exports.start = start;
