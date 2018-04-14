c1299d34ba89bb5761e762e0b1528ec2e3199f6e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-14 14:09:59 -0700

    added pathname

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const http = require('http');
+const url = require('url');
 
 function start() {
     function onRequest(request, response) {
-        console.log('Request received');
+        const pathname = url.parse(request.url).pathname;
+        console.log(`Request for ${pathname} received`);
+
+        route(pathname);
+
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('hello world');
         response.end();
