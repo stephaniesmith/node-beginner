53dfe1e5db690cd54b0b2cc8757a6d9484b9d016
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-15 12:24:15 -0700

    added timeout

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,13 @@ const exec = require('child_process').exec;
 function start(response) {
     console.log('Request handler start was called');
 
-    exec('ls -lah', (error, stdout, stderr) => {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write(stdout);
-        response.end();
-    });
+    exec('find /',
+        { timeout: 10000, maxBuffer: 20000*1024 },
+        (error, stdout, stderr) => {
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write(stdout);
+            response.end();
+        });
 }
 
 function upload(response) {
