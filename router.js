9c69a4d7dea176cb771e13cc6c9815ee275224a0
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-15 11:49:30 -0700

    upudated server and router

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 function route(handle, pathname) {
     console.log(`About to route a request for ${pathname}`);
     if (typeof handle[pathname] === 'function') {
-        return handle[pathname]();
+        handle[pathname]();
     } else {
         console.log(`No request handler found for ${pathname}`);
-        return '404 Not found';
+        // return '404 Not found';
     }
 }
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
         const pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} received`);
