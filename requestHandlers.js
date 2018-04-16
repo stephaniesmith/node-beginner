173cff653a3f57ff87210783925c7e11c52c6f2d
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-16 10:38:03 -0700

    updated html

--- requestHandlers.js
+++ requestHandlers.js
@@ -7,12 +7,14 @@ function start(response) {
 
     const body = '<html>'+
         '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
-        '</head>'+ '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
+        '<meta http-equiv="Content-Type" '+
+        'content="text/html; charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" enctype="multipart/form-data" '+
+        'method="post">'+
+        '<input type="file" name="upload" multiple="multiple">'+
+        '<input type="submit" value="Upload file" />'+
         '</form>'+
         '</body>'+
         '</html>';
